ee6fb30f270f5e5991daba508f83c94d3042ed3f
JonathanStewart2 <jonathan.stewart2@hotmail.com>
2022-10-07 10:51:18 +0100

    updating error handling file

--- error_handling_again/expressEH.js
+++ error_handling_again/expressEH.js
@@ -18,6 +18,12 @@ app.get('/getAll', (req,res,next) => {
     res.send(pokemon);
 })
 
+app.get('/find/:id', (req,res,next) => {
+    const id = req.params.id;
+    if (id >= pokemon.length) return next( {status: 404, message: "Invalid ID or ID does not exist"} );
+    res.send(pokemon[id]);
+})
+
 app.post('/addPokemon', (req,res,next) => {
     if (!req.body.name) return next({
         status: 404,
@@ -27,6 +33,13 @@ app.post('/addPokemon', (req,res,next) => {
     console.log(`Request received for ${newPokemon} to be added`);
     pokemon.push(newPokemon);
     res.send(`Success, ${newPokemon} added!`);
+})
+
+app.delete('/deletePokemon/:id', (req,res,next) => {
+    if (req.params.id >= pokemon.length) return next( {status: 404, message: "Cannot remove pokemon, ID doesn't exist"});
+    let removed = pokemon.splice(req.params.id, 1);
+    res.send(`${removed} has been removed from the pokemon list`);
+
 })
 
 // error handling
